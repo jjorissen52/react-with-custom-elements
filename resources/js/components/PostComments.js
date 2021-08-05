82d7ade3e151bfaaace1788d349d4f5e8e7d1a60
jjorissen52 <jjorissen52@gmail.com>
2021-08-05 10:06:27 -0500

    re-render on attribute change

--- resources/js/components/PostComments.js
+++ resources/js/components/PostComments.js
@@ -1,17 +1,34 @@
 import {createElement} from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import {render} from 'react-dom'
+import * as retargetEvents from 'react-shadow-dom-retarget-events';
 import {Comments} from './Comments'
 
 class PostComments extends HTMLElement {
-  connectedCallback () {
-    const props = Object.values(this.attributes).map(attribute => [attribute.name, attribute.value])
+    static get observedAttributes() {
+        return ['post']
+    }
 
-    render(createElement(Comments, Object.fromEntries(props)), this)
-  }
+    get props() {
+        return Object.fromEntries(Object.values(this.attributes).map(attribute => [attribute.name, attribute.value]))
+    }
 
-  disconnectedCallback () {
-    unmountComponentAtNode(Comments)
-  }
+    connectedCallback() {
+        this.mountPoint = document.createElement('span');
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.appendChild(this.mountPoint);
+        render(this.mRender(this.props), this.mountPoint)
+        retargetEvents(shadowRoot);
+    }
+
+    mRender(props) {
+        return createElement(Comments, props, createElement('slot'))
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (PostComments.observedAttributes.includes(name)) {
+            render(this.mRender(this.props), this.mountPoint)
+        }
+    }
 }
 
 customElements.define('post-comments', PostComments)
